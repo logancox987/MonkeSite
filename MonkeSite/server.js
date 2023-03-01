47a580ab434c04f7c4e6825620b39db899bbb889
MUKILTEO\1710091 <1710091@mukilteo.wednet.edu>
2023-03-01 10:20:05 -0800

    Add project files.

---
+++ MonkeSite/server.js
@@ -0,0 +1,19 @@
+'use strict';
+var http = require('http');
+var port = process.env.PORT || 8080;
+
+var http = require('http');
+var fs = require('fs');
+
+const PORT = 8080;
+
+fs.readFile('./index.html', function (err, html) {
+
+    if (err) throw err;
+
+    http.createServer(function (request, response) {
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        response.write(html);
+        response.end();
+    }).listen(PORT);
+});
