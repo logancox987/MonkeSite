06a7c13746e492e577754782a5eca3f23964c8da
MUKILTEO\1710091 <1710091@mukilteo.wednet.edu>
2023-03-07 10:20:26 -0800

    fixed it

--- MonkeSite/server.js
+++ MonkeSite/server.js
@@ -11,6 +11,7 @@ var fs = require('fs');
 var dt = require('./main');
 
 const PORT = 8080;
+const PORT2 = 8081;
 
 
 
@@ -26,5 +27,29 @@ fs.readFile('./index.html', function (err, html) {
     }).listen(PORT);
 });
 
+fs.readFile('./enterName.html', function (err, html) {
+
+    if (err) throw err;
+
+    http.createServer(function (request, response) {
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        response.write(html);
+        response.end();
+    }).listen(PORT2);
+});
+
+//fs.readFile('./enterName.html');
+//fs.writeFile('./enterName.html');
+
+//fs.readFile('./enterName.html', function (err, html) {
+
+//    if (err) throw err;
+
+//    http.createServer(function (request, response) {
+//        response.writeHeader(200, { "Content-Type": "text/html" });
+//        response.write(html);
+//        response.end();
+//    }).listen(PORT);
+//});
 
 
